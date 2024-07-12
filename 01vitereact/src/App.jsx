6b33e4410a90e236f532b8abf5fce9f0171e29bf
Arpit Gupta <arpitgpta08@gmail.com>
2024-07-12 12:14:48 +0530

    fixed engine

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Chai from "./chai"
+function App() {
+  const username = "chai aur code"
+// ab aise apn return ke andar ek hi chij likh parhe the to usse bachne ke liye hm ise bracket tag ke andar likhenge taki hm aur bh chije likh ske uske andar
+  return (
+    <>
+
+    <Chai/>
+    <h1>chai aur react {username}</h1>
+    <p>test para</p>
+    </>
+  )
+}
+
+export default App
